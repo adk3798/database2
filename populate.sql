b0511db2394239c933a0fc348d8b94cc2b923024
Adam King <kingamk3@gmail.com>
2020-02-24 20:53:36 -0500

    Initilazing database 2 project repository

---
+++ populate.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (email, password, name, phone) VALUES ('user1@gmail.com', 'password', 'name1', '999-999-9999');
+INSERT INTO users (email, password, name, phone) VALUES ('user2@gmail.com', 'password', 'name2', '999-999-9998');
+INSERT INTO users (email, password, name, phone) VALUES ('user3@gmail.com', 'password', 'name3', '999-999-9997');
